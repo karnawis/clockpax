5565c2939bd09311f59e20ae66a612a9d465832b
Sura <sk@sura.me>
2024-12-28 15:30:53 -0500

    added font face and updated tailwind styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,15 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      theme: {
+        extend: {
+          fontFamily: {
+            sans: ['facade-quest', 'sans-serif'],
+          },
+        }
+      }
+    },
   },
   plugins: [],
 }
