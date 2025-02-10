4acd624865fe12008ce09556c97e3fadb5d8ca8d
Sura <sk@sura.me>
2025-02-09 21:56:11 -0500

    removing extra space

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -2,7 +2,7 @@ const Footer = () => {
     const year = new Date().getFullYear();
     return (
         <footer>
-            <p className=" font-bold  text-slate-500">© { year } - sura.me -  All rights reserved</p>
+            <p className=" font-bold  text-slate-500">© { year } - sura.me - All rights reserved</p>
         </footer>
     )
 }
